b8516228a2eba20b32757aa33aa5ab64de5ed24d
Nils Knappmeier <n.knappmeier@i-views.de>
2016-01-12 22:08:43 +0100

    Initial check-in

---
+++ index.js
@@ -0,0 +1,17 @@
+/*!
+ * bootprint-integration-test <https://github.com/nknapp/bootprint-integration-test>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+'use strict'
+
+module.exports = bootprintIntegrationTest
+/**
+ * Describe your module here
+ * @public
+ */
+function bootprintIntegrationTest () {
+  // body
+}
---
+++ test/main-spec.js
@@ -0,0 +1,19 @@
+/*!
+ * bootprint-integration-test <https://github.com/nknapp/bootprint-integration-test>
+ *
+ * Copyright (c) 2016 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+// /* global it */
+// /* global xdescribe */
+// /* global xit */
+
+'use strict'
+
+var bootprintIntegrationTest = require('../')
+
+describe('bootprint-integration-test:', function () {
+  // body
+})
