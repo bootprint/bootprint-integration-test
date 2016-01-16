b4e80f9ed6275e5f04abf42f05d13ef1be5223b7
Nils Knappmeier <n.knappmeier@i-views.de>
2016-01-16 09:45:14 +0100

    bootprint-swagger is not bootprint-openapi

--- run-test.sh
+++ run-test.sh
@@ -1,14 +1,15 @@
 #!/usr/bin/env bash
 
 npm install -g bootprint || exit 1
+npm install -g bootprint-openapi || exit 1
 npm install -g bootprint-swagger || exit 1
 
 
 function testBootprint() {
     
     rm -rf target
-    echo running: bootprint swagger "$1" target
-    bootprint swagger "$1" target || exit 1
+    echo running: bootprint "$1" "$2" target
+    bootprint "$1" "$2" target || exit 1
     if [ ! -f "target/index.html" ] ; then
         echo No target/index.html found
         exit 1
@@ -21,5 +22,7 @@ function testBootprint() {
     echo done 
 }
 
-testBootprint http://petstore.swagger.io/v2/swagger.json 
-testBootprint minimal.yaml
+testBootprint swagger http://petstore.swagger.io/v2/swagger.json 
+testBootprint swagger minimal.yaml
+testBootprint openapi http://petstore.swagger.io/v2/swagger.json 
+testBootprint openapi minimal.yaml
